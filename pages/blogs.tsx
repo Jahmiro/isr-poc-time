53e9c35a1e75106447cc27cea30fdc0543a84170
Jahmiro <Jahmiro000@hotmail.com>
2024-07-22 13:45:40 +0200

    server

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,7 +1,6 @@
 import Navigation from "@/components/navigation";
 import { GetStaticProps } from "next";
 import Link from "next/link";
-import { useEffect, useState } from "react";
 
 type Blog = {
   id: number;
@@ -14,19 +13,6 @@ type Props = {
 };
 
 const BlogsPage = ({ blogs }: Props) => {
-  const [localBlogs, setLocalBlogs] = useState<Blog[]>(blogs);
-
-  useEffect(() => {
-    const cachedBlogs = localStorage.getItem('blogs');
-    if (cachedBlogs) {
-      setLocalBlogs(JSON.parse(cachedBlogs));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem('blogs', JSON.stringify(localBlogs));
-  }, [localBlogs]);
-
   return (
     <>
       <Navigation />
@@ -38,8 +24,8 @@ const BlogsPage = ({ blogs }: Props) => {
             </h2>
           </div>
           <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {localBlogs.length > 0 ? (
-              localBlogs.map((blog) => (
+            {blogs.length > 0 ? (
+              blogs.map((blog) => (
                 <Link href={`/blogs/${blog.id}`} key={blog.id}>
                   <article className="flex max-w-xl flex-col items-start justify-between border rounded-lg overflow-hidden">
                     <div className="group relative p-6">
@@ -76,15 +62,15 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
       props: {
         blogs,
       },
-      revalidate: 10,
+      revalidate: 10, 
     };
   } catch (error) {
     console.error("Error fetching blogs:", error);
     return {
       props: {
-        blogs: [],
+        blogs: [], 
       },
-      revalidate: 10,
+      revalidate: 10, 
     };
   }
 };
