c55d9f3569986419dee294e8aa939f7c6096298d
Jahmiro <Jahmiro000@hotmail.com>
2024-07-23 09:52:20 +0200

    Update blogs.tsx

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -21,7 +21,7 @@ const BlogsPage = ({ blogs }: Props) => {
           <div className="mx-auto max-w-2xl lg:mx-0">
             <h2 className="text-3xl font-bold tracking-tight text-tertiary-800 sm:text-4xl">
               Blog
-            </h2>
+            </h2> 
           </div>
           <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
             {blogs.length > 0 ? (
